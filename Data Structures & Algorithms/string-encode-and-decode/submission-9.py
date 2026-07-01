eac06b08d88722ba6abb294b4b1e1da1450408f2
Ryan Shankar <137834625+Shankary23@users.noreply.github.com>
2026-07-01 13:13:44 -0700

    Add: string-encode-and-decode - submission-9

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-9.py
@@ -0,0 +1,30 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for i in strs:
+            word_len = len(i)
+            res+= str(word_len) + "$"
+            res+= i
+        return res
+    def decode(self, s: str) -> List[str]:
+        res = []
+        word = ""
+        if not s:
+            return []
+        i = 0
+        j = 0
+        while i< len(s):
+            j=i
+            while s[j] != "$":
+                j+=1
+            pos = int(s[i:j])
+            word+= s[j+1 : j+1+pos]
+            res.append(word)
+            i = j+1 + pos
+            word = ""
+        return res
+
+
+
+        
